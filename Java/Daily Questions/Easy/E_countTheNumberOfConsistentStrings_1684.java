dc3b7123d041f2520389c0a375390e245ae11785
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2024-09-12 11:29:26 -0700

    Create E_countTheNumberOfConsistentStrings_1684.java

---
+++ Java/Daily Questions/Easy/E_countTheNumberOfConsistentStrings_1684.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int[] map = new int[26];
+        for (Character c : allowed.toCharArray()){
+            map[c - 'a']++;
+        }
+
+        int numConsistentStrings = 0;
+        for (String word : words){
+            for (int i = 0; i <= word.length(); i++){
+                if (i == word.length()){
+                    numConsistentStrings++;
+                }
+                else if (map[word.charAt(i) - 'a'] == 0){
+                    break;
+                }
+            }
+        }
+        return numConsistentStrings;
+    }
+}
