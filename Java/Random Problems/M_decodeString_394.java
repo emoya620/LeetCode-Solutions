83e41321eeeade9e58972cfe75d7386fa01eff77
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2024-09-16 17:55:43 -0700

    Create M_decodeString_394.java

---
+++ Java/Random Problems/M_decodeString_394.java
@@ -0,0 +1,24 @@
+class Solution {
+    private int pos = 0;
+    public String decodeString(String s) {
+        StringBuilder sb = new StringBuilder();
+        String num = "";
+        for (int i = pos; i < s.length(); i++) {
+            if (s.charAt(i) != '[' && s.charAt(i) != ']' && !Character.isDigit(s.charAt(i))) {
+                sb.append(s.charAt(i));
+            } else if (Character.isDigit(s.charAt(i))) {
+                num += s.charAt(i);
+            } else if (s.charAt(i) == '[') {
+                pos = i + 1;
+                String next = decodeString(s);
+                for (int n = Integer.valueOf(num); n > 0; n--) sb.append(next);
+                num = "";
+                i = pos;
+            } else if (s.charAt(i) == ']') {
+                pos = i;
+                return sb.toString();
+            }
+        }
+        return sb.toString();
+    }
+}
