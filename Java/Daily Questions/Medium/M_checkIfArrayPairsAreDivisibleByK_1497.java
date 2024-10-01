efb6e4e98ecc61eb58b99397e54514626c5cef2b
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2024-10-01 10:41:58 -0700

    Create M_checkIfArrayPairsAreDivisibleByK_1497.java

---
+++ Java/Daily Questions/Medium/M_checkIfArrayPairsAreDivisibleByK_1497.java
@@ -0,0 +1,18 @@
+// Solution taken from LeetCode forums
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        int[] freq = new int[k];
+        
+        for (int num : arr) {
+            freq[((num % k) + k) % k]++;
+        }
+        
+        if (freq[0] % 2 != 0) return false;
+        
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) return false;
+        }
+        
+        return true;
+    }
+}
