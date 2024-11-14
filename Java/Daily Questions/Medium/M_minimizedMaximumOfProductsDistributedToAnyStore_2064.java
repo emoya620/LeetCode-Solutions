c7070ebcab3bb1fe59074ee6cfce7bc871980810
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2024-11-14 12:05:25 -0800

    Create M_minimizedMaximumOfProductsDistributedToAnyStore_2064.java

---
+++ Java/Daily Questions/Medium/M_minimizedMaximumOfProductsDistributedToAnyStore_2064.java
@@ -0,0 +1,43 @@
+class Solution {
+    // Time Complexity: O(nlogk) | Space Complexity: O(1)
+    public int minimizedMaximum(int n, int[] quantities) {
+        int left = 0;
+        int right = 0;
+
+        for (int quantity : quantities){
+            right = Math.max(right, quantity);
+        }
+
+        while (left < right){
+            int middle = (left + right) / 2;
+            if (canDistribute(middle, quantities, n)){
+                right = middle;
+            }
+            else{
+                left = middle + 1;
+            }
+        }
+        return left;
+    }
+
+    public boolean canDistribute(int x, int[] quantities, int n){
+        int j = 0;
+        int remaining = quantities[j];
+
+        for (int i = 0; i < n; i++){
+            if (remaining <= x){
+                j++;
+                if (j == quantities.length){
+                    return true;
+                }
+                else{
+                    remaining = quantities[j];
+                }
+            }
+            else{
+                remaining -= x;
+            }
+        }
+        return false;
+    }
+}
