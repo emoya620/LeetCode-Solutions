ec7735f94d487b3e0bc746dfa8a6f1dc7eeae535
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2024-12-22 20:14:04 -0800

    Create M_lowestCommonAncestorOfABinaryTreeII_1644.java

---
+++ Java/Weekly Questions/Medium/M_lowestCommonAncestorOfABinaryTreeII_1644.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    boolean nodesFound = false;
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ans = dfs(root, p, q);
+        return nodesFound ? ans : null;
+    }
+
+    private TreeNode dfs(TreeNode node, TreeNode p, TreeNode q) {
+        if (node == null) return null;
+
+        TreeNode left = dfs(node.left, p, q);
+        TreeNode right = dfs(node.right, p, q);
+
+        int conditions = 0;
+        if (node == p || node == q) conditions++;
+        if (left != null) conditions++;
+        if (right != null) conditions++;
+        if (conditions == 2) nodesFound = true;
+
+        if ((left != null && right != null) || node == p || node == q){
+            return node;
+        } 
+
+        return left != null ? left : right;
+    }
+}
