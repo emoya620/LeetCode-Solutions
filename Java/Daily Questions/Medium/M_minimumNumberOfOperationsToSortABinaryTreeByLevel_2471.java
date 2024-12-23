731f81e2e9a72204d01ffd279705a59b0397dfff
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2024-12-22 20:08:18 -0800

    Create M_minimumNumberOfOperationsToSortABinaryTreeByLevel_2471.java

---
+++ Java/Daily Questions/Medium/M_minimumNumberOfOperationsToSortABinaryTreeByLevel_2471.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    final int SHIFT = 20;
+    final int MASK = 0xFFFFF;
+
+    public int minimumOperations(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        int swaps = 0;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            long[] nodes = new long[levelSize];
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                nodes[i] = ((long) node.val << SHIFT) + i;
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            Arrays.sort(nodes);
+
+            for (int i = 0; i < levelSize; i++) {
+                int origPos = (int) (nodes[i] & MASK);
+                if (origPos != i) {
+                    long temp = nodes[i];
+                    nodes[i--] = nodes[origPos];
+                    nodes[origPos] = temp;
+                    swaps++;
+                }
+            }
+        }
+        return swaps;
+    }
+}
