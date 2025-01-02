039d73ee6b86a49f4e4eeed648135be25f04462a
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-01-01 19:58:43 -0500

    Create E_performStringShifts_1427.java

---
+++ Java/Weekly Questions/Easy/E_performStringShifts_1427.java
@@ -0,0 +1,16 @@
+class Solution {
+    // Time Complexity: O(N + L) | Space Complexity: O(L)
+    public String stringShift(String s, int[][] shift) {
+        int leftShifts = 0;
+        for (int[] move : shift) {
+            if (move[0] == 1) {
+                move[1] = -move[1];
+            }
+            leftShifts += move[1];
+        }
+
+        leftShifts = Math.floorMod(leftShifts, s.length());
+        s = s.substring(leftShifts) + s.substring(0, leftShifts);
+        return s;
+    }
+}
