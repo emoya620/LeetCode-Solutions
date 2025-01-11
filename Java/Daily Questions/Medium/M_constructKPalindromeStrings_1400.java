d5ec68e5264fdd7c33271f752c86640d4388453c
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-01-11 15:53:52 -0500

    Create M_constructKPalindromeStrings_1400.java

---
+++ Java/Daily Questions/Medium/M_constructKPalindromeStrings_1400.java
@@ -0,0 +1,14 @@
+class Solution {
+    // Time Complexity: O(n) | Space Complexity: O(1)
+    public boolean canConstruct(String s, int k) {
+        if (s.length() < k) return false;
+        if (s.length() == k) return true;
+
+        int oddCount = 0;
+        for (char chr : s.toCharArray()) {
+            oddCount ^= 1 << (chr - 'a');
+        }
+
+        return Integer.bitCount(oddCount) <= k;
+    }
+}
