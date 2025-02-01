5e2117ec07b6949e48c9f2c44a368038fadcbc7a
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-02-01 13:56:59 -0500

    Create E_specialArrayI_3151.java

---
+++ Java/Daily Questions/Easy/E_specialArrayI_3151.java
@@ -0,0 +1,11 @@
+// Time Complexity: O(n) | Space Complexity: O(1)
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i-1] % 2 == nums[i] % 2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
