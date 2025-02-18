eb02e9e57c772e60a72c24b24fb7c4e51cea7937
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-02-18 10:36:48 -0500

    Create M_constructSmallestNumberFromDIString_2375.java

---
+++ Java/Daily Questions/Medium/M_constructSmallestNumberFromDIString_2375.java
@@ -0,0 +1,18 @@
+// Time Complexity: O(n) | Space Complexity: O(n)
+class Solution {
+    public String smallestNumber(String pattern) {
+        StringBuilder result = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i <= pattern.length(); i++){
+            stack.push(i + 1);
+
+            if (i == pattern.length() || pattern.charAt(i) == 'I'){
+                while (!stack.isEmpty()){
+                    result.append(stack.pop());
+                }
+            }
+        }
+        return result.toString();
+    }
+}
