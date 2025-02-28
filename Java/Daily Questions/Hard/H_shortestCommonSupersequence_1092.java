a2b91868770a7be8465819c815d3a6743ffe20fa
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-02-28 11:43:34 -0500

    Create H_shortestCommonSupersequence_1092.java

---
+++ Java/Daily Questions/Hard/H_shortestCommonSupersequence_1092.java
@@ -0,0 +1,32 @@
+// Time Complexity: O(n * m * (n + m)) | Space Complexity: O(m * (n + m))
+class Solution {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int str1Length = str1.length();
+        int str2Length = str2.length();
+
+        String[] prevRow = new String[str2Length + 1];
+        for (int col = 0; col <= str2Length; col++){
+            prevRow[col] = str2.substring(0, col);
+        }
+
+        for (int row = 1; row <= str1Length; row++){
+            String[] curRow = new String[str2Length + 1];
+            curRow[0] = str1.substring(0, row);
+
+            for (int col = 1; col <= str2Length; col++){
+                if (str1.charAt(row - 1) == str2.charAt(col - 1)){
+                    curRow[col] = prevRow[col - 1] + str1.charAt(row - 1);
+                }
+                else{
+                    String pickS1 = prevRow[col];
+                    String pickS2 = curRow[col - 1];
+
+                    curRow[col] = (pickS1.length() < pickS2.length()) ? pickS1 + str1.charAt(row-1) : pickS2 + str2.charAt(col-1);
+                }
+            }
+            prevRow = curRow;
+        }
+
+        return prevRow[str2Length];
+    }
+}
