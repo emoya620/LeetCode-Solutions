4fad5324e52c8d9e7ebbfb1b58c68e48e0c2beef
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-04-08 23:23:56 -0400

    Create E_minimumOperationsToMakeArrayValuesEqualToK_3375.java

---
+++ Java/Daily Questions/Easy/E_minimumOperationsToMakeArrayValuesEqualToK_3375.java
@@ -0,0 +1,14 @@
+// Time complexity: O(n) | Space Complexity: O(n)
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Set<Integer> st = new HashSet<>();
+        for (int x : nums) {
+            if (x < k) {
+                return -1;
+            } else if (x > k) {
+                st.add(x);
+            }
+        }
+        return st.size();
+    }
+}
