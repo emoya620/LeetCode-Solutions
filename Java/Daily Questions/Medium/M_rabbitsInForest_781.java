891261ea88b3780ca2125a06a560fce93f069e2b
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-04-20 00:01:54 -0400

    Create M_rabbitsInForest_781.java

---
+++ Java/Daily Questions/Medium/M_rabbitsInForest_781.java
@@ -0,0 +1,12 @@
+// Time Complexity: O(n) | Space Complexity: O(n)
+class Solution {
+    public int numRabbits(int[] answers) {
+        int[] count = new int[1000];
+        for (int x: answers) count[x]++;
+
+        int ans = 0;
+        for (int k = 0; k < 1000; ++k)
+            ans += Math.floorMod(-count[k], k+1) + count[k];
+        return ans;
+    }
+}
