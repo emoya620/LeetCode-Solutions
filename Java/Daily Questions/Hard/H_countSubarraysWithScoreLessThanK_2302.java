2336e644e2d0ea8f994709ce8da5afbcb3e4571c
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-04-27 22:27:54 -0400

    Create H_countSubarraysWithScoreLessThanK_2302.java

---
+++ Java/Daily Questions/Hard/H_countSubarraysWithScoreLessThanK_2302.java
@@ -0,0 +1,16 @@
+// Time Complexity: O(n) | Space Complexity: O(1)
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        int n = nums.length;
+        long res = 0, total = 0;
+        for (int i = 0, j = 0; j < n; j++) {
+            total += nums[j];
+            while (i <= j && total * (j - i + 1) >= k) {
+                total -= nums[i];
+                i++;
+            }
+            res += j - i + 1;
+        }
+        return res;
+    }
+}
