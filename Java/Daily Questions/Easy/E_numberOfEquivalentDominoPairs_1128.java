74ef0e24841754324795b8f3f02a1eb1bd7b9578
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-05-04 14:47:22 -0400

    Create E_numberOfEquivalentDominoPairs_1128.java

---
+++ Java/Daily Questions/Easy/E_numberOfEquivalentDominoPairs_1128.java
@@ -0,0 +1,15 @@
+// Time Complexity: O(n) | Space Complexity: O(1)
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] num = new int[100];
+        int ret = 0;
+        for (int[] domino : dominoes) {
+            int val = domino[0] < domino[1]
+                ? domino[0] * 10 + domino[1]
+                : domino[1] * 10 + domino[0];
+            ret += num[val];
+            num[val]++;
+        }
+        return ret;
+    }
+}
