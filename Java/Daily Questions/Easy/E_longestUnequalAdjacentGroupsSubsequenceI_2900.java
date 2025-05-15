ef98e4e192f94dd287c861d985ef8d61481ab0bf
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2025-05-14 22:06:42 -0400

    Create E_longestUnequalAdjacentGroupsSubsequenceI_2900.java

---
+++ Java/Daily Questions/Easy/E_longestUnequalAdjacentGroupsSubsequenceI_2900.java
@@ -0,0 +1,13 @@
+// Time Complexity: O(n) | Space Complexity: O(1)
+class Solution {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> ans = new ArrayList<>();
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || groups[i] != groups[i - 1]) {
+                ans.add(words[i]);
+            }
+        }
+        return ans;
+    }
+}
